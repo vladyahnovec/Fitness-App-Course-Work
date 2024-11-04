fdf572c145417be248b8c7c7d7657644e42b33ac
Vlad Yahnovec <vladyahnovec@gmail.com>
2024-11-04 14:31:59 +0300

    Project initialisation

---
+++ Fitness App/Fitness App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fitness App
+//
+//  Created by Круглич Влад on 4.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fitness App/Fitness App/Fitness_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_AppApp.swift
+//  Fitness App
+//
+//  Created by Круглич Влад on 4.11.24.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
